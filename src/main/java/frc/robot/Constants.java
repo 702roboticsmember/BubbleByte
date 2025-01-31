508c6ed36fe5a3d82b022bc6040840130bd3ffe3
702computer <team702robotics@gmail.com>
2025-01-30 19:18:30 -0800

    fixed

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -1,5 +1,6 @@
 package frc.robot;
 
+import static frc.lib.util.COTSTalonFXSwerveConstants.SDS.MK4i.Falcon500;
 import static frc.lib.util.COTSTalonFXSwerveConstants.SDS.MK4i.KrakenX60;
 
 
@@ -324,7 +325,7 @@ public final class Constants {
     public static final class ElevatorConstants{
 
         public static final int Motor1ID = 0;
-        public static final int Motor2ID = 1;
+        public static final int Motor2ID = 0;
         public static final double ConversionConstant = 0.0;
         public static final InvertedValue MotorInverted = InvertedValue.Clockwise_Positive;
         public static final NeutralModeValue MotorMode = NeutralModeValue.Brake;
